1e159f4a2bc8f1c0c201c9e7d1e4c3bc2c26cb81
Vergangenheit <lorenzo.ostano@gmail.com>
2021-05-01 09:48:28 +0200

    pushing the rest

---
+++ script_download_data.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+
+def download_electricity():
+    """Downloads electricity dataset from UCI repository."""
+
+    url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/00321/LD2011_2014.txt.zip'
