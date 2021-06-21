ba4add3916e17a91ccd73a4bbbaf017c573ba6cd
Vergangenheit <lorenzo.ostano@gmail.com>
2021-06-21 07:31:37 +0200

    checkoing out

--- inference/gcp_request.py
+++ inference/gcp_request.py
@@ -11,13 +11,13 @@ except:
     print('No ".env" file or python-dotenv not installed... Using default env variables...')
 
 
-MODEL_NAME: str = "tft_sorgenia"
+MODEL_NAME: str = "tft_sorgenia_logs"
 PROJECT_ID = "omnienergy-316210"
 sample_path: str = r'C:\Users\Lorenzo\PycharmProjects\TFT\outputs\data\sorgenia_wind\data\sorgenia_wind\data\sorgenia_wind_inference_sample.csv'
 
 instances: List = pd.read_csv(sample_path).values.tolist()
 service: Resource = googleapiclient.discovery.build('ml', 'v1')
-name: str = 'projects/{}/models/{}/versions/{}'.format(PROJECT_ID, MODEL_NAME, 'v3')
+name: str = 'projects/{}/models/{}/versions/{}'.format(PROJECT_ID, MODEL_NAME, 'v2')
 
 response: Dict = service.projects().predict(
     name=name,
