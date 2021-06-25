0550ee0390a01f6afa4a5013d87c4fc59147af58
Loloheia <lorenzo.ostano@omninext.it>
2021-06-25 11:01:50 +0200

    sorgenia

--- expt_settings/configs.py
+++ expt_settings/configs.py
@@ -91,4 +91,4 @@ class ExperimentConfig(object):
             'sotavento': SotaventoFormatter,
         }
 
-        return data_formatter_class[self.experiment](self.model_folder)
+        return data_formatter_class[self.experiment]()
