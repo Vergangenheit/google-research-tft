d1d6f69447b70074899b415264a4a646f0a90107
Loloheia <lorenzo.ostano@omninext.it>
2021-10-07 12:42:14 +0200

    tweaked sorgenia preprocessing function to add latest meteomatics data

--- script_download_data.py
+++ script_download_data.py
@@ -10,7 +10,7 @@ from sqlalchemy.engine import Engine, Connection
 from sqlalchemy import create_engine
 from sqlalchemy.pool import NullPool
 from os import getenv
-from typing import Optional
+from typing import Optional, List
 from datetime import datetime
 from etl.ETL import db_connection, group_hourly, extract_weather, etl_plant, etl_weather
 
@@ -146,16 +146,16 @@ def preprocess_erg_wind(datapath: str, config: ExperimentConfig):
     print('Done.')
 
 
-def merge_df(energy: DataFrame, weather: DataFrame) -> DataFrame:
+def merge_df(energy: DataFrame, weather: DataFrame, col_drop: List) -> DataFrame:
     df: DataFrame = energy.merge(weather, left_on=['time', 'plant_name_up'],
                                  right_on=['timestamp_utc', 'plant_name_up'])
-    df.drop(['timestamp_utc', 'id'], axis=1, inplace=True)
+    df.drop(col_drop, axis=1, inplace=True)
     df = df.sort_values(by=['plant_name_up', 'time'], ascending=True, ignore_index=True)
 
     return df
 
 
-def preprocess_sorgenia(weather_source: str, config: ExperimentConfig, get_df: bool = False) -> Optional[DataFrame]:
+def preprocess_sorgenia_full(config: ExperimentConfig, get_df: bool = False) -> Optional[DataFrame]:
     """end to end etl function from database to df
     :param : weather_source (str) whether to use meteomatics or copernicus
     :param : config (ExperimentConfig) class for experiment configuration
@@ -164,15 +164,34 @@ def preprocess_sorgenia(weather_source: str, config: ExperimentConfig, get_df: b
     engine: Engine = db_connection()
     sql_energy: str = "SELECT * FROM sorgenia_energy"
     energy_df: DataFrame = pd.read_sql_query(sql_energy, con=engine)
-    energy_grouped: DataFrame = group_hourly(energy_df)
-    if weather_source == 'copernicus':
-        weather_df: DataFrame = extract_weather(f"SELECT * FROM sorgenia_weather_{weather_source}", engine)
-    else:
-        weather_df: DataFrame = extract_weather(f"SELECT * FROM sorgenia_weather", engine)
-    assert weather_df.plant_name_up.value_counts().tolist().sort() == energy_grouped.plant_name_up.unique().tolist().sort()
-    df: DataFrame = merge_df(energy_grouped, weather_df)
-    # adding features
+    energy_grouped: DataFrame = group_hourly(energy_df, 'start_date_utc', 'plant_name_up')
+    # extract weather mm historical
+    mm_query: str = "SELECT * FROM sorgenia_weather"
+    weather_df: DataFrame = extract_weather(mm_query, engine)
+    # INFER THE DATES GAP BETWEEN Energy and Weather dfs
+    upper: str = weather_df['timestamp_utc'].min().strftime('%Y-%m-%d %H:%M:%S')
+    lower: str = energy_grouped['time'].min().strftime('%Y-%m-%d %H:%M:%S')
+    cop_sql: str = f"SELECT * FROM sorgenia_weather_copernicus WHERE timestamp_utc >= '{lower}' and timestamp_utc < '{upper}'"
+    weather_remain: DataFrame = extract_weather(cop_sql, engine)
+    #  STACK WEATHER df together
+    weather: DataFrame = pd.concat([weather_df, weather_remain], axis=0)
+    weather.sort_values(by='timestamp_utc', ascending=True, inplace=True)
+    # INFER THE DATES GAP BETWEEN Energy and Weather dfs
+    lower: str = weather['timestamp_utc'].max().strftime('%Y-%m-%d %H:%M:%S')
+    upper: str = energy_grouped['time'].max().strftime('%Y-%m-%d %H:%M:%S')
+    # extract weather meteomatics "recent"
+    mm_sql: str = f"SELECT * FROM meteomatics_weather WHERE timestamp_utc >= '{lower}' and timestamp_utc < '{upper}'"
+    weather_remain2: DataFrame = extract_weather(mm_sql, engine)
+    weather_remain2.rename(columns={'plant_code': 'plant_name_up'}, inplace=True)
+    weather_remain2 = weather_remain2[list(weather.columns)]
+    weather_remain2.drop(['id'], axis=1, inplace=True)
+    weather.drop(['id'], axis=1, inplace=True)
+    weather: DataFrame = pd.concat([weather, weather_remain2], axis=0)
+    weather.sort_values(by='timestamp_utc', ascending=True, inplace=True)
+    df: DataFrame = merge_df(energy_grouped, weather, ['timestamp_utc'])
+    print(df.tail())
     timestamp_s: Series = df['time'].map(datetime.timestamp)
+
     day: int = 24 * 60 * 60
     year: float = 365.2425 * day
 
@@ -195,6 +214,7 @@ def preprocess_sorgenia(weather_source: str, config: ExperimentConfig, get_df: b
     df['categorical_hour']: Series = df['hour'].copy()
 
     # save df to csv file
+    # df = df[df['plant_name_up'] == 'UP_MPNTLCDMRN_1']
     df.to_csv(config.data_csv_path, index=False)
     print(f'Saved in {config.data_csv_path}')
     print('Done.')
@@ -223,7 +243,7 @@ def preprocess_sorgenia_cop_mm(config: ExperimentConfig, get_df: bool = False) -
     #  STACK WEATHER df together
     weather: DataFrame = pd.concat([weather_df, weather_remain], axis=0)
     weather.sort_values(by='timestamp_utc', ascending=True, inplace=True)
-    df: DataFrame = merge_df(energy_grouped, weather)
+    df: DataFrame = merge_df(energy_grouped, weather, ['timestamp_utc', 'id'])
 
     timestamp_s: Series = df['time'].map(datetime.timestamp)
 
@@ -294,4 +314,4 @@ if __name__ == "__main__":
     # expt_config = ExperimentConfig('erg_wind', './outputs/data/erg_wind')
     # preprocess_sorgenia(r'C:\Users\Lorenzo\PycharmProjects\TFT\outputs\data', expt_config)
     expt_config = ExperimentConfig('sorgenia_wind', './outputs/data/sorgenia_wind')
-    preprocess_sorgenia_cop_mm(expt_config)
+    preprocess_sorgenia_full(expt_config)
